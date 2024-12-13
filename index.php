9c456b55e0e0223eaf882f3759ca32ce52ab45d7
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2024-12-13 11:39:44 +0100

    creer la fonctionnalité d'afficher les packets et la fonctionnalité d'ajouter un packet

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!DOCTYPE html class="dark">
 <html lang="en">
 
 <head>
@@ -9,27 +9,71 @@
 </head>
 
 <body>
-
    <?php
-   include("./components/component.php");
+   include("./components/packages_table.php");
+   include("./components/add_form.php");
+   // connecter avec mysql
    $dsn = 'mysql:host=localhost;dbname=package_manager';
    $user = 'root';
    $password = '';
    $options = array(
       PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
    );
+   $db = new PDO($dsn, $user, $password);
+   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+   if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+      $name = $_POST["name"];
+      $description = $_POST["description"];
+      $author = $_POST["author"];
+      $email = $_POST["email"];
+      $bio = $_POST["bio"];
+
+      try {
+         $query = "INSERT INTO authors (name, email, bio) VALUES (:author, :email, :bio);";
+         $stmt = $db->prepare($query);
+         $stmt->execute([
+            ':author' => $author,
+            ':email' => $email,
+            ':bio' => $bio
+         ]);
+
+         $author_id = $db->lastInsertId();
+         $query = "INSERT INTO packages (name, description, author_id) VALUES (:name, :description, :author_id)";
+         $stmt = $db->prepare($query);
+         $stmt->execute([
+            ':name' => $name,
+            ':description' => $description,
+            ':author_id' => $author_id
+         ]);
+      } catch (PDOException $e) {
+         echo "Erreur lors de l'ajout : " . $e->getMessage();
+      }
+   };
 
    try {
-      $db = new PDO($dsn, $user, $password);
-      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $query = "SELECT 
+                  p.id,
+                  p.name,
+                  p.description,
+                  a.name AS author,
+                  GROUP_CONCAT(DISTINCT v.version_number ORDER BY v.version_number) AS versions FROM packages p
+                  LEFT JOIN authors a ON p.author_id = a.id
+                  LEFT JOIN versions v ON p.id = v.package_id
+                  GROUP BY p.id, p.name, p.description, a.name";
 
-      echo 'connected';
+      $stmt = $db->prepare($query);
+      $stmt->execute();
+      $packages = $stmt->fetchAll(PDO::FETCH_ASSOC);
    } catch (\Throwable $th) {
       echo '' . $th->getMessage() . '';
    }
-   $template = template();
-   echo "<h1>$template</h1>";
+   // afficher les packages
+   echo packages_table($packages);
+   // ajouter le formulaire
+   echo add_form();
    ?>
+   <script src="./main.js"></script>
 </body>
 
 </html>
